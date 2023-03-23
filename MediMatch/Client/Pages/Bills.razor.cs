5abaf15b8d7862c47c5be2ef633aa06557041c7a
Shimaya <963147@corp.trizetto.com>
2023-03-22 20:11:38 -0700

    added bills

---
+++ MediMatch/Client/Pages/Bills.razor.cs
@@ -0,0 +1,26 @@
+﻿using MediMatch.Server.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Json;
+
+namespace MediMatch.Client.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public HttpClient Http { get; set; } = new();
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+        public Bill Bill { get; set; } = new Bill();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var UserAuth = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User.Identity;
+            if (UserAuth is not null && UserAuth.IsAuthenticated)
+            {
+                Bill = await Http.GetFromJsonAsync<Bill>("api/get-movies");
+
+            }
+        }
+    }
+}
