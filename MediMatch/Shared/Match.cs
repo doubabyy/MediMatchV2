82651a4184cc58830e3bc274f76528b028da0213
Shimaya <963147@corp.trizetto.com>
2023-03-30 10:45:38 -0700

    Match class

---
+++ MediMatch/Shared/Match.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediMatch.Shared
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public string PatientId { get; set; }
+        public string DoctorId { get; set; }
+        public DateTime RequestedAt { get; set; }
+        public DateTime? AcceptedAt { get; set; }
+        public bool Accepted { get; set; }
+    }
+}
