1832217ffe6ccad5b14073cb6a3f94cc2281c61b
Shou Shimaya <shoushimaya@gmail.com>
2023-04-05 21:04:47 -0700

    cleaned up

--- MediMatch/Server/Controllers/MessageController.cs
+++ MediMatch/Server/Controllers/MessageController.cs
@@ -11,36 +11,7 @@ using System.Threading.Tasks;
 
 namespace MediMatch.Server.Controllers
 {
-    /*
-      
-    public class MessageController : Controller
-    {
-        private readonly ApplicationDbContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
-
-        public MessageController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
-        {
-            _context = context;
-            _userManager = userManager;
-        }
-
-        [HttpGet]
-        [Route("api/GetMessages")]
-        public async Task<ActionResult<List<Message>>> GetMessages()
-        {
-            var messages = await (from u in _context.Users
-                                  join m in _context.Messages on u.Id equals m.MessageToID
-                                  where u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier)
-                                  select
-                                  m
-
-                                  ).ToListAsync();
-            return Ok(messages);
-        }
-
-    }
-
-    */
+ 
     [Authorize]
     [ApiController]
     public class MessageController : ControllerBase
