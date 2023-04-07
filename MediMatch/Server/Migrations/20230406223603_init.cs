420ea9dc582b515fe3ca693f1fef02183a8ac300
Shou Shimaya <shoushimaya@gmail.com>
2023-04-06 18:54:19 -0700

    started admin

--- MediMatch/Server/Migrations/20230406223603_init.cs
+++ MediMatch/Server/Migrations/20230406223603_init.cs
@@ -155,12 +155,22 @@ namespace MediMatch.Server.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     MessageTxt = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     MessageDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    MessageFromID = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    MessageToID = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    MessageFromID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    MessageToID = table.Column<string>(type: "nvarchar(450)", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Messages", x => x.MessageId);
+                table.ForeignKey(
+                    name: "FK_Messages_AspNetUsers_MessageFromID",
+                    column: x => x.MessageFromID,
+                    principalTable: "AspNetUsers",
+                    principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Messages_AspNetUsers_MessageToID",
+                    column: x => x.MessageToID,
+                    principalTable: "AspNetUsers",
+                    principalColumn: "Id");
                 });
 
             migrationBuilder.CreateTable(
